941c658167dbde504eb60adbaaaa92c7f57cfb5a
Haolan Li <haolan@ucsb.edu>
2021-11-05 17:30:28 -0700

    hl - pass tests.

--- sub_fetch.test.js
+++ sub_fetch.test.js
@@ -2,7 +2,7 @@ import SubFetch from "./sub_fetch";
 
 test("Something fetch CC", () => {
     let sub_fetch = new SubFetch("en");
-    sub_fetch.fetch("Y4EABGSesk").then(
+    sub_fetch.fetch_subtitle("Y4EABGSesk").then(
         data => {
             expect(data).toBe([]);
         }
