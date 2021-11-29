330340da75f9e8eb5bd064fd7b03521fecde4198
kaleb <jinjieguo@ucsb.edu>
2021-11-29 15:29:09 -0800

    Some changes.

--- popup.js
+++ popup.js
@@ -1,16 +1,82 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var playButton = document.getElementById('play');
-    playButton.addEventListener('click', function() {
-      
-      chrome.tabs.getSelected(null,function(tab){
-      alert('Play button clicked');
-      });
-    }, false);
+var synth = window.speechSynthesis;
 
-    var settingsButton = document.getElementById('settings');
-    settingsButton.addEventListener('click', function() {
+var inputForm = document.querySelector('form');
+var voiceSelect = document.querySelector('select');
 
-      window.location.href='settings.html';
+var pitch = document.querySelector('#pitch');
+var pitchValue = document.querySelector('.pitch-value');
+var rate = document.querySelector('#rate');
+var rateValue = document.querySelector('.rate-value');
 
-  }, false);
-}, false);
+var voices = [];
+
+function populateVoiceList() {
+  voices = synth.getVoices().sort(function (a, b) {
+      const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+      if ( aname < bname ) return -1;
+      else if ( aname == bname ) return 0;
+      else return +1;
+  });
+  var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+  voiceSelect.innerHTML = '';
+  for(let i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+    
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+  voiceSelect.selectedIndex = selectedIndex;
+}
+
+populateVoiceList();
+
+
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+
+pitch.onchange = function() {
+  pitchValue.textContent = pitch.value;
+}
+
+rate.onchange = function() {
+  rateValue.textContent = rate.value;
+}
+
+
+
+function popup() {
+  console.log(voiceSelect.selectedIndex);
+  console.log(pitch.valueAsNumber);
+  console.log(rate.valueAsNumber);
+
+  let storg = {};
+  storg["voice"] = voiceSelect.selectedIndex;
+  storg["pitch"] = pitch.valueAsNumber;
+  storg["rate"] = rate.valueAsNumber;
+  chrome.storage.local.set(storg, function() {
+    console.log("popup(): ", storg);
+});
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("DOM loaded");
+  document.getElementById("play").addEventListener("click", function() {
+    popup();
+  });
+});
+
+
+
+
+voiceSelect.selectedIndex 
+pitch
+rate
--- speech.js
+++ speech.js
@@ -1,15 +1,37 @@
-let synth = window.speechSynthesis;
+let voice;
+let rate;
+let pitch;
+
+let voices = synth.getVoices().sort(function (a, b) {
+  const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+  if ( aname < bname ) return -1;
+  else if ( aname == bname ) return 0;
+  else return +1;
+});
+
+chrome.storage.local.get(["voice", "pitch", "rate"], function(result){
+  voice = result.voice;
+  rate = result.rate;
+  pitch = result.pitch;
+});
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         switch (request.command) {
             case "speak": {
-                const line = request.data.line;
-
-                console.log("Speak", line);
-
-                var utterance = new SpeechSynthesisUtterance(line);
-                synth.speak(utterance);
+                chrome.storage.local.get(["voice", "pitch", "rate"], function(result){
+                  voice = result.voice;
+                  rate = result.rate;
+                  pitch = result.pitch;
+                });
+               
+                console.log("Speak", request.data.line);
+                console.log("voice:", voice)
+                var utterThis = new SpeechSynthesisUtterance(request.data.line);
+                utterThis.voice = voice;
+                utterThis.pitch = pitch;
+                utterThis.rate = rate;
+                synth.speak(utterThis);
             }
         }
 
