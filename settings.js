709657901264404fc99d340096be85f50675b120
huntermassey33 <77125370+huntermassey33@users.noreply.github.com>
2021-12-01 19:37:09 -0800

    Integrated new features with a new update button

--- settings.js
+++ settings.js
@@ -1,21 +1,94 @@
+var synth = window.speechSynthesis;
+
+var inputForm = document.querySelector('form');
+var voiceSelect = document.querySelector('select');
+
+var pitch = document.querySelector('#pitch');
+var pitchValue = document.querySelector('.pitch-value');
+var rate = document.querySelector('#rate');
+var rateValue = document.querySelector('.rate-value');
+
+var voices = [];
+
+function populateVoiceList() {
+  voices = synth.getVoices().sort(function (a, b) {
+      const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+      if ( aname < bname ) return -1;
+      else if ( aname == bname ) return 0;
+      else return +1;
+  });
+  var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+  voiceSelect.innerHTML = '';
+  for(let i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+    
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+  voiceSelect.selectedIndex = selectedIndex;
+}
+
+populateVoiceList();
+
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+pitch.onchange = function() {
+  pitchValue.textContent = pitch.value;
+}
+
+rate.onchange = function() {
+  rateValue.textContent = rate.value;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 
+    console.log("DOM loaded");
+
+    //update button for pitch, accent, and speed
+    var updateV = document.getElementById("updateV");
+    updateV.addEventListener("click", function() {
+      alert("Voice changed!"); 
+      chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        chrome.tabs.sendMessage( tabs[0].id,
+          { command: "voice",
+            data: { 
+              voice: voiceSelect.selectedIndex,
+              pitch: pitch.valueAsNumber, 
+              rate: rate.valueAsNumber
+          }, 
+          function(response) {
+            console.log(response.farewell);
+          }
+        });
+      });
+    });
+
     //functionality for speech censoring
-    var updateButton = document.getElementById('update');
+    var updateC = document.getElementById('updateC');
     var old_txt = document.getElementById('old').value;
     var new_txt = document.getElementById('new').value;
     var censorBox = document.getElementById('censor');
-    var censor = document.getElementById('censor').checked
+    var censor = document.getElementById('censor').checked;
     censorBox.addEventListener('click', function() {
       censor = !censor
     });
-    updateButton.addEventListener('click', function() {
+
+    //update button for censoring
+    updateC.addEventListener('click', function() {
       old_txt = document.getElementById('old').value;
       new_txt = document.getElementById('new').value;
       alert(old_txt + " -> " + new_txt + "; censor: " + censor);
       chrome.tabs.query({active: true, currentWindow: true}, tabs => {
       chrome.tabs.sendMessage( tabs[0].id,
-        { command: "update", 
+        { command: "censor", 
           data: { old: old_txt , new: new_txt, cen: censor}
         }, 
         function(response) {
@@ -32,47 +105,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
     });
 
-    //accent control
-    var accentButton = document.getElementById('accent');
-    accentButton.addEventListener('click', function() {
-
-      document.getElementById("myDropdown").classList.toggle("show");
-
-    });
-
-    var englishButton = document.getElementById('english');
-    englishButton.addEventListener('click', function() {
-      accentButton.innerHTML = englishButton.innerHTML;
-    });
-
-    var spanishButton = document.getElementById('spanish');
-    spanishButton.addEventListener('click', function() {
-      accentButton.innerHTML = spanishButton.innerHTML
-    });
-
-    var frenchButton = document.getElementById('french');
-    frenchButton.addEventListener('click', function() {
-      accentButton.innerHTML = frenchButton.innerHTML
-    });
-
-    var russianButton = document.getElementById('russian');
-    russianButton.addEventListener('click', function() {
-      accentButton.innerHTML = russianButton.innerHTML
-    });
-
-    
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.button') && !event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-}, false);
+});
--- speech.js
+++ speech.js
@@ -1,15 +1,18 @@
-let synth = window.speechSynthesis;
-
 let old_txt = [];
 let new_txt = [];
 let censor = false;
 let censored = ["[ __ ]", "fuck", "shit", "bitch", "ass", 
-"cunt", "dick", "motherfucker", "slut", "prick", "asshole"]
+"cunt", "dick", "slut", "prick"]
+
+let voice = 0;
+let rate = 1;
+let pitch = 1;
+var synth = window.speechSynthesis;
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         switch (request.command) {
-            case "update": {
+            case "censor": {
                 if (request.data.old !== "") {
                     old_txt.push(request.data.old);
                     new_txt.push(request.data.new);
@@ -17,10 +20,20 @@ chrome.runtime.onMessage.addListener(
                 } else if (request.data.new !== "") {
                     alert("Update failed: please enter a word to censor.")
                 } else {
-                    alert("Update successful!")
+                    alert ("Update successful!")
                 }
                 censor = request.data.cen;
+                break;
             }
+
+            case "voice": {
+                voice = request.data.voice;
+                rate = request.data.rate;
+                pitch = request.data.pitch;
+                alert("Update successful!")
+                break;
+            }
+
             case "speak": {
                 let line = request.data.line;
                 if (censor === true){
@@ -43,11 +56,13 @@ chrome.runtime.onMessage.addListener(
                 
                 console.log("Speak", line);
 
-                var utterance = new SpeechSynthesisUtterance(line);
-                synth.speak(utterance);
+                var utterThis = new SpeechSynthesisUtterance(line);
+                utterThis.voice = synth.getVoices()[voice];
+                utterThis.pitch = pitch;
+                utterThis.rate = rate;
+                synth.speak(utterThis);
             }
         }
-
         sendResponse({ok: true});
     }
 );
